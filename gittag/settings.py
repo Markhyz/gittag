0d44f965f8a66c2443794041c28540998cde0016
framos <framos@fesctecnologia.com.br>
2021-09-28 00:49:03 -0300

    fixed django cors to handle heroku

--- gittag/settings.py
+++ gittag/settings.py
@@ -39,8 +39,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
-CORS_ALLOWED_ORIGINS = ['http://localhost',
-                        'http://localhost:8080', '.herokuapp.com']
+CORS_ALLOWED_ORIGINS = ['http://localhost', 'http://localhost:8080']
+
+CORS_ALLOWED_ORIGIN_REGEXES = [r'*.herokuapp.com']
 
 # Application definition
 
